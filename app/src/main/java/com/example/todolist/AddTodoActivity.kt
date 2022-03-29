5fed25538aec85e8cbcd0c895a9548fcee1a20e0
JungWo Lee <leejungwoo0203@gmail.com>
2022-03-29 18:01:12 +0900

    뷰홀더 패턴을 사용하여 RecyclerView 구현

--- app/src/main/java/com/example/todolist/AddTodoActivity.kt
+++ app/src/main/java/com/example/todolist/AddTodoActivity.kt
@@ -2,10 +2,63 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.todolist.databinding.ActivityAddTodoBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.ToDoDao
+import com.example.todolist.db.TodoEntity
 
 class AddTodoActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAddTodoBinding
+    lateinit var db : AppDatabase
+    lateinit var todoDao : ToDoDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_todo)
+
+        binding = ActivityAddTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getToDoDao()
+
+        binding.btnCompletion.setOnClickListener{
+            insertToDo()
+        }
+    }
+
+    private fun insertToDo() {
+        val todoTitle = binding.edtTitle.text.toString() // 할일의 제목
+        var todoImportance = binding.radioGroup.checkedRadioButtonId
+
+        //어떤 버튼이 눌렸는지 확인하고 값을 지정
+        when(todoImportance){
+            R.id.btn_high ->{
+                todoImportance = 1
+            }
+            R.id.btn_middle ->{
+                todoImportance = 2
+            }
+            R.id.btn_low ->{
+                todoImportance = 3
+            }
+            else ->{
+                todoImportance = -1
+            }
+        }
+        // 중요도가 선택되지 않거나 제목이 작성되지 않는지 체크
+        if (todoImportance == -1 || todoTitle.isBlank()){
+            Toast.makeText(this,"모든 항목을 채워주세요",
+            Toast.LENGTH_SHORT).show()
+        }else {
+            Thread{
+                todoDao.insertTodo(TodoEntity(null, todoTitle, todoImportance))
+                runOnUiThread{ // 아래 작업은 UI 스레드에서 실행해줘야함
+                    Toast.makeText(this,"추가되었습니다.",Toast.LENGTH_SHORT).show()
+                    finish() // AddTodoActivity 종료, 다시 MainActivity 로 돌아감
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Dao
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.ToDoDao
+import com.example.todolist.db.TodoEntity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var db : AppDatabase
+    private lateinit var todoDao: ToDoDao
+    private lateinit var todoList : ArrayList<TodoEntity>
+    private lateinit var adapter : ToDoRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener{
+            val intent = Intent(this, AddTodoActivity::class.java)
+            startActivity(intent)
+        }
+
+        // DB에 인스턴스를 가져오고 DB 작업을 할 수 있는 DAO 를 가져온다.
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getToDoDao()
+
+        getAllTodoList()
+    }
+
+    private fun getAllTodoList() {
+        Thread{
+            todoList = ArrayList(todoDao.getAll())
+            setRecyclerView()
+        }
+    }
+
+    private fun setRecyclerView(){
+        runOnUiThread {
+            adapter = ToDoRecyclerViewAdapter(todoList) //어뎁터 객체 할당
+            binding.recyclerView.adapter = adapter
+            //리사이클러뷰가 어댑터로 위에서 만든 어댑터 설정
+            binding.recyclerView.layoutManager = LinearLayoutManager(this)
+            //레이아웃 메니저 설정
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        getAllTodoList()
     }
 }
